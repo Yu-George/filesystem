37493a07e61982bd3aca423fabad1af35ad6ba08
DsinCDev <geoyu99@gmail.com>
2024-03-07 10:27:01 -0800

    added all commands with cmdline interface

--- main.py
+++ main.py
@@ -1,11 +1,58 @@
 class Directory:
-    def __init__(self, name, parent, path):
+    def __init__(self, name, parent):
         self.name = name
         self.subdirectories = {}
         self.files = []
         self.parent = parent
-        self.path = path
 
 class FileSystem:
     def __init__(self):
-        self.root = Directory("/", None, "/")
+        self.root = Directory("/", None)
+        self.fullpath = "/"
+    def cd(self, path):
+        if path == "..":
+            # Check if current directory is root
+            if self.current_dir.name != "/":
+                self.current_dir = self.current_dir.parent
+        else:
+            if path in self.current_dir.subdirectories:
+                self.current_dir = self.current_dir.subdirectories[path]
+            else:
+                print("Directory does not exist")
+
+    def ls(self):
+        print("Directories:")
+        for directory in self.current_dir.subdirectories:
+            print(directory)
+        print("Files:")
+        for file in self.current_dir.files:
+            print(file)
+
+    def mkdir(self, directory_name):
+        new_directory = Directory(directory_name, self.current_dir, self.current_dir.path)
+        self.current_dir.subdirectories[directory_name] = new_directory
+
+    def touch(self, file_name):
+        self.current_dir.files.append(file_name)
+
+    def main(self):
+        self.current_dir = self.root
+        while True:
+            command = input(f"{self.current_dir.name}$ ")
+            if command == "exit":
+                break
+            elif command.split()[0] == "cd":
+                self.cd(command.split()[1])
+            elif command == "ls":
+                self.ls()
+            elif command.split()[0] == "mkdir":
+                self.mkdir(command.split()[1])
+            elif command.split()[0] == "touch":
+                self.touch(command.split()[1])
+            else:
+                print("Invalid command")
+
+
+if __name__ == "__main__":
+    fs = FileSystem()
+    fs.main()
